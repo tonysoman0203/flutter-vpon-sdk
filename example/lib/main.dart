3756f61a698326bde3c74e587b0bcb7f670e1291
Desson Chan <dessonchan@linkingapp.com>
2023-02-02 11:25:11 +0800

    - update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,72 +24,85 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     vponSDK.init();
-    interstitialAd = VponInterstitialAd(
-        licenseKey: "8a80854b6a90b5bc016ad81a98cf652e",
-        platform: "TW",
-        // testAd: true,
-        eventListener: VponAdListener(
-            onAdLoaded: () {
-              print("Ad Loaded");
-            },
-            onAdOpened: () {
-              print("Ad Opened");
-            },
-            onAdClosed: () {
-              print("Ad Colsed");
-            },
-            onAdClicked: () {
-              print("Ad Clicked");
-            },
-            onAdFailedToLoad: (dynamic error) {
-              print("Ad Error : $error");
-            },
-            onAdLeftApplication: () {
-              print("Ad Left App");
-            }
-        )
-    );
   }
 
-  Future<void> createVponInterstitialAd() async {
+  createVponInterstitialAd(BuildContext context) async {
     try {
+      interstitialAd = VponInterstitialAd(
+          licenseKey: "8a80854b6a90b5bc016ad81a98cf652e",
+          platform: "TW",
+          testAd: true,
+          eventListener: VponAdListener(
+              onAdLoaded: () {
+                print("onAdLoaded");
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("onAdLoaded")));
+              },
+              onAdOpened: () {
+                print("onAdOpened");
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("onAdOpened")));
+              },
+              onAdClosed: () {
+                print("onAdClosed");
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("onAdClosed")));
+              },
+              onAdClicked: () {
+                print("onAdClicked");
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("onAdClicked")));
+              },
+              onAdFailedToLoad: (dynamic error) {
+                print("onAdFailedToLoad: $error}");
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("onAdFailedToLoad \n ${error.toString()}")));
+              },
+              onAdLeftApplication: () {
+                print("onAdLeftApplication");
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("onAdLeftApplication")));
+              }
+          )
+      );
       await vponSDK.loadVponInterstitialAd(interstitialAd: interstitialAd!);
     } catch (error) {
-      print(error);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("createVponInterstitialAd(Error): ${error.toString()}")));
     }
     if (!mounted) return;
   }
 
-  Future<void> showAds() async {
-    print("show");
+  Future<void> showAds(BuildContext context) async {
     try {
       await vponSDK.showVponInterstitialAd(interstitialAd: interstitialAd!);
     } catch (error) {
-      print(error);
-      print("Failed to showAds");
+      print("showAds(Error): ${error.toString()}");
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("showAds(Error): ${error.toString()}")));
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Vpon SDK Example'),
-        ),
-        body: Column(
-          children: [
-            ElevatedButton(
-                onPressed: () async => createVponInterstitialAd(),
-                child: Text("Create Vpon Ads!")
-            ),
-            ElevatedButton(
-                onPressed: () async => showAds(),
-                child: Text("Show Vpon Ads!")
-            )
-          ],
-        ),
-      ),
+        home: Builder(
+          builder: (BuildContext context) {
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Vpon SDK Example'),
+              ),
+              body: Column(
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        createVponInterstitialAd(context);
+                      },
+                      child: Text("Create Vpon Ads!")
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        showAds(context);
+                      },
+                      child: Text("Show Vpon Ads!")
+                  )
+                ],
+              ),
+            );
+          },
+        )
     );
   }
 }
